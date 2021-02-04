60416174cc3908c83976c40aab8ac72988a19315
Giorgio Silvestris <giorgio.silvestris@gmail.com>
2021-02-04 14:26:57 +0100

    update

--- src/test/java/org/dew/test/TestJDBCTracer.java
+++ src/test/java/org/dew/test/TestJDBCTracer.java
@@ -1,5 +1,6 @@
 package org.dew.test;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -10,6 +11,8 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 public class TestJDBCTracer extends TestCase {
+  
+  protected static final String DB_NAME = "test";
   
   public TestJDBCTracer(String testName) {
     super(testName);
@@ -26,6 +29,7 @@ public class TestJDBCTracer extends TestCase {
   }
   
   public void executeQuery() throws Exception {
+    
     Connection conn = null;
     Statement  stm  = null;
     ResultSet  rs   = null;
@@ -47,10 +51,24 @@ public class TestJDBCTracer extends TestCase {
       if(stm  != null) try { stm.close();  } catch(Exception ignore) {}
       if(conn != null) try { conn.close(); } catch(Exception ignore) {}
     }
+    
+    File fileP = new File(DB_NAME + ".properties");
+    if(fileP.exists()) {
+      boolean resDelete = fileP.delete();
+      String removed = resDelete ? "removed." : "NOT removed.";
+      System.out.println(fileP.getAbsolutePath() + " " + removed);
+    }
+    
+    File fileS = new File(DB_NAME + ".script");
+    if(fileS.exists()) {
+      boolean resDelete = fileS.delete();
+      String removed = resDelete ? "removed." : "NOT removed.";
+      System.out.println(fileS.getAbsolutePath() + " " + removed);
+    }
   }
   
   public static Connection getConnection() throws Exception {
     Class.forName("org.dew.jdbc.drivers.HSQLDBDriverTracer");
-    return DriverManager.getConnection("jdbc:dew:mem:test", "SA", "");
+    return DriverManager.getConnection("jdbc:dew:mem:" + DB_NAME, "SA", "");
   }
 }
